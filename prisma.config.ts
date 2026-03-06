40a6fc9ecbf03580da037c40d6e25cfcca856e18
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-06 17:13:08 +0800

    feat: define db schema

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import { Base } from './.nuxt/components.d'
+import "dotenv/config"
+import { defineConfig } from "prisma/config"
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"]
+  },
+})
