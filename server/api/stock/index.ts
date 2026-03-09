3f7e76d04f7688bd0fd7b08d1dc56b26eedc4173
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-09 17:10:18 +0800

    refactor: move bigint serialization to prisma util

--- server/api/stock/index.ts
+++ server/api/stock/index.ts
@@ -1,9 +1,58 @@
-(BigInt.prototype as any).toJSON = function () {
-  return this.toString()
-}
-
 export default defineEventHandler(async (event) => {
   try {
+    const { year, month } = getQuery(event)
+
+    const date = new Date()
+    const yearInt = year ? parseInt(year.toString(), 10) : date.getFullYear()
+    const monthInt = month ? parseInt(month.toString(), 10) : date.getMonth() + 1
+
+    if (isNaN(yearInt) || isNaN(monthInt)) {
+      setResponseStatus(event, 400)
+
+      return {
+        message: "Year dan month harus berupa angka"
+      }
+    }
+
+    if (monthInt < 1 || monthInt > 12) {
+      setResponseStatus(event, 400)
+
+      return {
+        message: "Month harus antara 1 - 12"
+      }
+    }
+
+    const info = await prisma.info.findFirst({
+      where: {
+        month: monthInt,
+        year: yearInt,
+      }
+    })
+
+    if (!info) {
+      setResponseStatus(event, 404)
+
+      return {
+        message: 'Data tidak tersedia'
+      }
+    }
+
+    if (yearInt > info.year || (yearInt === info.year && monthInt > info.month)) {
+      setResponseStatus(event, 404)
+
+      return {
+        message: 'Data belum tersedia'
+      }
+    }
+
+    if (yearInt < 2026 || (yearInt === 2026 && monthInt < 2)) {
+      setResponseStatus(event, 404)
+
+      return {
+        message: 'Data dibawah februari 2026 tidak tersedia'
+      }
+    }
+
     return await prisma.stock.findMany({
       select: {
         ticker: true,
@@ -18,8 +67,14 @@ export default defineEventHandler(async (event) => {
             scrip: true,
             totalHoldingShare: true,
             percentage: true,
+          },
+          where: {
+            infoId: info.id,
           }
         },
+      },
+      orderBy: {
+        ticker: 'asc'
       },
     })
   } catch (error) {
--- server/utils/prisma.ts
+++ server/utils/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "../generated/prisma/client"
 
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString()
+}
+
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 })
