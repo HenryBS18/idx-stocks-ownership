90e891167aef000d874f4aa66c1981873eb22202
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-10 01:28:40 +0800

    chore: configure nuxt ui and tailwindcss
    - @nuxt/ui
    - tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: false
+  ssr: process.env.NODE_ENV !== 'development',
+  modules: ['@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
 })
