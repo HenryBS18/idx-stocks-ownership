1dc3e5dfc7c83538f91c4433537cb04b638857af
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-17 01:13:43 +0800

    feat: add float and free float data and add route cache

--- server/api/stock/index.ts
+++ server/api/stock/index.ts
@@ -1,59 +1,53 @@
-export default defineEventHandler(async (event) => {
+export default defineCachedEventHandler(async (event) => {
   try {
     const { year, month } = getQuery(event)
 
-    const date = new Date()
-    const yearInt = year ? parseInt(year.toString(), 10) : date.getFullYear()
-    const monthInt = month ? parseInt(month.toString(), 10) : date.getMonth() + 1
+    let yearInt: number
+    let monthInt: number
 
-    if (isNaN(yearInt) || isNaN(monthInt)) {
-      setResponseStatus(event, 400)
+    if (!year && !month) {
+      const latestInfo = await prisma.info.findFirst({
+        orderBy: [
+          { year: 'desc' },
+          { month: 'desc' }
+        ]
+      })
 
-      return {
-        message: "Year dan month harus berupa angka"
+      if (!latestInfo) {
+        setResponseStatus(event, 404)
+        return { message: 'Data tidak tersedia' }
       }
-    }
 
-    if (monthInt < 1 || monthInt > 12) {
-      setResponseStatus(event, 400)
+      yearInt = latestInfo.year
+      monthInt = latestInfo.month
+    } else {
+      yearInt = parseInt(year!.toString(), 10)
+      monthInt = parseInt(month!.toString(), 10)
 
-      return {
-        message: "Month harus antara 1 - 12"
+      if (isNaN(yearInt) || isNaN(monthInt)) {
+        setResponseStatus(event, 400)
+        return { message: "Year dan month harus berupa angka" }
+      }
+
+      if (monthInt < 1 || monthInt > 12) {
+        setResponseStatus(event, 400)
+        return { message: "Month harus antara 1 - 12" }
       }
     }
 
     const info = await prisma.info.findFirst({
       where: {
-        month: monthInt,
         year: yearInt,
+        month: monthInt
       }
     })
 
     if (!info) {
       setResponseStatus(event, 404)
-
-      return {
-        message: 'Data tidak tersedia'
-      }
+      return { message: 'Data tidak tersedia' }
     }
 
-    if (yearInt > info.year || (yearInt === info.year && monthInt > info.month)) {
-      setResponseStatus(event, 404)
-
-      return {
-        message: 'Data belum tersedia'
-      }
-    }
-
-    if (yearInt < 2026 || (yearInt === 2026 && monthInt < 2)) {
-      setResponseStatus(event, 404)
-
-      return {
-        message: 'Data dibawah februari 2026 tidak tersedia'
-      }
-    }
-
-    return await prisma.stock.findMany({
+    const stocksQuery = await prisma.stock.findMany({
       select: {
         ticker: true,
         name: true,
@@ -77,6 +71,30 @@ export default defineEventHandler(async (event) => {
         ticker: 'asc'
       },
     })
+
+    const stocks = stocksQuery.map((s) => {
+      const float = parseFloat(
+        s.stockInvestor
+          .reduce((acc, curr) => acc + parseFloat(curr.percentage.toString()), 0)
+          .toFixed(2)
+      )
+
+      const freeFloat = parseFloat((100 - float).toFixed(2))
+
+      return {
+        ticker: s.ticker,
+        name: s.name,
+        float,
+        freeFloat,
+        investors: s.stockInvestor,
+      }
+    })
+
+    return {
+      lastUpdated: info.idxLastUpdated,
+      data: stocks
+    }
+
   } catch (error) {
     if (error instanceof Error) {
       setResponseStatus(event, 500)
@@ -86,4 +104,6 @@ export default defineEventHandler(async (event) => {
       }
     }
   }
+}, {
+  maxAge: 60 * 60
 })
