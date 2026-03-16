514211c708f061c2524deecf555f7f14c44fdc8f
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-16 17:46:14 +0800

    feat: implement filter and sort stock

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { TabsItem } from "@nuxt/ui"
+import type { Stock } from "~/types"
+
 const StockAccordion = defineAsyncComponent(() =>
   import("~/components/StockAccordion.vue")
 )
@@ -9,12 +11,54 @@ const tabItems: TabsItem[] = [
   { label: 'Investor', slot: 'investor', icon: 'i-lucide-user' },
 ]
 
-const stocks = ref([])
-const showStockAccordion = ref(false)
+const stocks = ref<Stock[]>([])
+const showStockAccordion = ref<boolean>(false)
+const search = ref<string>("")
+const sortField = ref<'alphabet' | 'freeFloat'>('alphabet')
+const sortOrder = ref<'asc' | 'desc'>('asc')
+
+const toggleAlphabetSort = () => {
+  sortField.value = 'alphabet'
+  sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc'
+}
+
+const toggleFreeFloatSort = () => {
+  sortField.value = 'freeFloat'
+  sortOrder.value = sortOrder.value === 'asc' ? 'desc' : 'asc'
+}
+
+const filteredStocks = computed<Stock[]>(() => {
+  let result = [...stocks.value]
+
+  if (search.value) {
+    const q = search.value.toLowerCase()
+
+    result = result.filter(stock =>
+      stock.ticker.toLowerCase().includes(q) ||
+      stock.name.toLowerCase().includes(q)
+    )
+  }
+
+  result.sort((a, b) => {
+    let compare = 0
+
+    if (sortField.value === 'alphabet') {
+      compare = a.ticker.localeCompare(b.ticker)
+    }
+
+    if (sortField.value === 'freeFloat') {
+      compare = Number(a.freeFloat) - Number(b.freeFloat)
+    }
+
+    return sortOrder.value === 'asc' ? compare : -compare
+  })
+
+  return result
+})
 
 onMounted(() => {
   requestAnimationFrame(async () => {
-    const data = await $fetch('/api/stock')
+    const data = await $fetch<Stock[]>('/api/stock')
     stocks.value = data
     showStockAccordion.value = true
   })
@@ -33,13 +77,29 @@ onMounted(() => {
       }">
         <template #stock>
           <div class="px-4 py-2">
-            <UInput icon="i-lucide-search" placeholder="Cari kode saham, emiten..." :ui="{ leadingIcon: 'size-4' }" />
+            <div class="flex space-x-10">
+              <UInput v-model="search" icon="i-lucide-search" placeholder="Cari kode saham, emiten..." :ui="{ leadingIcon: 'size-4' }" />
+
+              <div class="flex">
+                <UButton label="Alphabet" :trailing-icon="sortField === 'alphabet' && sortOrder === 'asc'
+                  ? 'i-lucide-arrow-up-a-z'
+                  : 'i-lucide-arrow-down-z-a'
+                  " variant="outline" class="rounded-tr-none rounded-br-none cursor-pointer" :active="sortField === 'alphabet'"
+                  active-variant="solid" @click="toggleAlphabetSort" />
+
+                <UButton label="Free Float (%)" :trailing-icon="sortField === 'freeFloat' && sortOrder === 'asc'
+                  ? 'i-lucide-arrow-up-0-1'
+                  : 'i-lucide-arrow-down-1-0'
+                  " variant="outline" class="rounded-tl-none rounded-bl-none cursor-pointer" :active="sortField === 'freeFloat'"
+                  active-variant="solid" @click="toggleFreeFloatSort" />
+              </div>
+            </div>
 
             <div v-if="!showStockAccordion && stocks.length === 0" class="mt-6 space-y-3">
               <USkeleton class="w-full h-16 rounded-lg" v-for="i in 20" :key="i" />
             </div>
 
-            <StockAccordion v-else :stocks="stocks" />
+            <StockAccordion v-else :stocks="filteredStocks" />
           </div>
         </template>
 
