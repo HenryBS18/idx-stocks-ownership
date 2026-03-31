0a441c9fae4796519a210bf5601429a3667fa6fe
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-31 16:47:23 +0800

    chore: add dependencies
    - @pinia/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: process.env.NODE_ENV !== 'development',
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
   nitro: {
     preset: 'vercel',
