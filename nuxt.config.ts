ff88f88603109b7f3e1b6be883af10c7cc15e567
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-03-31 21:20:30 +0800

    feat: force light mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: process.env.NODE_ENV !== 'development',
-  modules: ['@nuxt/ui', '@pinia/nuxt'],
+  modules: [
+    '@nuxt/ui',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
   css: ['~/assets/css/main.css'],
   nitro: {
     preset: 'vercel',
+  },
+  colorMode: {
+    preference: 'light',
+    fallback: 'light',
+    storageKey: 'nuxt-color-mode',
+    disableTransition: false
   },
 })
