f7d0cdf47ece0a8b667f9531d9be88ab8f2b77d6
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-04-06 21:22:45 +0800

    feat: create get info api

---
+++ server/api/info/index.ts
@@ -0,0 +1,37 @@
+export default defineEventHandler(async (event) => {
+  try {
+    const token = getQuery(event).token?.toString()
+
+    const tokenVerified = verifyToken(token)
+
+    if (!tokenVerified) {
+      setResponseStatus(event, 401)
+
+      return { message: 'Unauthorized: Token tidak valid.' }
+    }
+
+    const infos = (await prisma.info.findMany({
+      orderBy: [
+        { year: 'desc' },
+        { month: 'desc' }
+      ],
+    },
+    )).map((info, i) => {
+      const dateString = getLastDate(info.month, info.year)
+      const label = `${dateString}${i === 0 ? ' (Terbaru)' : ''}`
+      const value = `${info.year}-${info.month}`
+
+      return { label, value }
+    })
+
+    return infos
+  } catch (error) {
+    if (error instanceof Error) {
+      setResponseStatus(event, 500)
+
+      return {
+        message: error.message
+      }
+    }
+  }
+})
