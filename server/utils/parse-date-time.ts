3ee6a0db381f15f040d89e2adb11244b13451eb7
Henry Bintang Setiawan <110516086+HenryBS18@users.noreply.github.com>
2026-07-28 21:21:27 +0800

    feat: validate date format in parseDateTime

--- server/utils/parse-date-time.ts
+++ server/utils/parse-date-time.ts
@@ -1,6 +1,10 @@
 import { monthsInNumber } from "./constants"
 
+const FORMAT_RE = /^\d{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}$/
+
 export const parseDateTime = (datetime: string) => {
+  if (!FORMAT_RE.test(datetime)) throw createError({ statusCode: 400, statusMessage: 'Invalid date format, expected "DD Mon YYYY"' })
+
   const [_, monthNameString, yearString] = datetime.split(" ")
 
   const month = monthsInNumber[monthNameString!]!
